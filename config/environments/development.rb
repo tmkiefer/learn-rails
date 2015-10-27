f8025e0e7486be44e2d1b79e448ffcc04b84ae4d
kiefer <kiefer.timothy@gmail.com>
2015-10-27 08:01:58 +0000

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
+  
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
